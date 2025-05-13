ddf0c489ab097406cc46a3e4e65c28651806099d
Abdul Halim <abxlim@users.noreply.github.com>
2025-05-13 19:06:18 +0700

    update: enhance styling and structure of the homepage
    - Memperbarui gaya CSS untuk header, logo, dan navigasi.
    - Menambahkan konten baru untuk bagian rumah, termasuk slider gambar.
    - Menghapus referensi font yang tidak digunakan.

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let indexBanner = 0;
+
+function nextBanner() {
+  indexBanner += 1;
+  showBanner();
+}
+
+function showBanner() {
+  const bannerList = document.getElementsByClassName("slide");
+
+  // Check if indexBanner is greater than the length of the bannerList
+  if (indexBanner > bannerList.length - 1) {
+    indexBanner = 0;
+  }
+
+  // Hide banner with looping
+  for (let i = 0; i < bannerList.length; i++) {
+    bannerList[i].style = "display: none";
+  }
+
+  // Show the banner
+  bannerList[indexBanner].style = "display: block";
+}
+
+showBanner();
+
+// Set interval to change the banner every 3 seconds
+setInterval(() => {
+  nextBanner();
+}, 3000);
+// Function to toggle the menu
